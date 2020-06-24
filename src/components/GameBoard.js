852b2fdf781dfbcfa9e8ea6f81f805132cfbd526
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-06-24 10:11:05 -0500

    rules updated

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -193,6 +193,7 @@ function GameBoard() {
                         </AppBar>
                         <div className="game-rules">
                             <h3>The rules of Conway's Game of Life are simple:</h3>
+                            <p>The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, live or dead, (or populated and unpopulated, respectively). Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent.</p>
                             <div>- Any live cell with fewer than two live neighbors dies, as if by underpopulation.</div>
                             <div>- Any live cell with two or three live neighbors lives on to the next generation.</div>
                             <div>- Any live cell with more than three live neighbors dies, as if by overpopulation.</div>
